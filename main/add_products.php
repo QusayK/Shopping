8751be1a15c6c943b02db1b4c7b545431ebb90fd
khalayleh <171034@ppu.edu.ps>
2020-06-08 22:30:12 +0300

    navbar items

--- main/add_products.php
+++ main/add_products.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['login'])) {
+
+        header("Location: login.php");
+        die();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
