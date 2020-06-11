ad2965a69d95f13409412d6ad03e6c711e50afb8
Qusay <khalayleh.qusay@gmail.com>
2020-06-11 12:11:35 +0300

    db

--- config/shopping.sql
+++ config/shopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2020 at 10:48 AM
+-- Generation Time: Jun 11, 2020 at 11:01 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.31
 
